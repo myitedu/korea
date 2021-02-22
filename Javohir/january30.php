df187b78a1f591cb3c847b2659c741e2d849fc16
Javokhirbek Tukhtasinov <javohir.manchester@gmail.com>
2021-02-22 14:03:34 +0900

    some files added

---
+++ Javohir/january30.php
@@ -0,0 +1,26 @@
+<?php
+
+$first_name = "Max";
+$last_name = "Smith";
+$age = "99";
+$place = "seul";
+$programmingLanguage = "php";
+$ism = "murod";
+$dob = 1990;
+// $currentYear = date( format:'Y');
+// data types
+// String
+// Integer
+// Float (floating point numbers - also called double)
+// Boolean
+// Array
+// Object
+// NULL
+// Resource
+
+// echo strrev("Hello world   ")
+$a = 123123;
+$d = date('W');
+echo $d;
+
+?>
